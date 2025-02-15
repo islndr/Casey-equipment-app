97aca3fb189c2d695c004aad95bfdf48e1dc0807
Jason Ostrander <jasonpostrander@gmail.com>
2025-02-15 10:31:28 -0700

    Tabs, Dashboard, Login are styled. Login is functional. Routing is all public. No Database setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
-import { AppComponent } from '../components/app/app.component'; // Import AppComponent
+import { AppComponent } from '../app/app.component'; // Import AppComponent
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { routes } from './app.routes';  // Import your routes
 
